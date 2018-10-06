4eb9cf4bf51077872d43374c0b9cf15255cf88ed
manhys01 <manh.yoshi@gmail.com>
2018-10-06 19:52:14 +0700

    Revert "Revert "Manh - Update""
    This reverts commit 6f65bdd649a65e8b0be8a147a93e681f6134ff07.

---
+++ src/main/java/poly/agile/webapp/exception/DuplicatePhoneNumberException.java
@@ -0,0 +1,10 @@
+package poly.agile.webapp.exception;
+
+public class DuplicatePhoneNumberException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+}
